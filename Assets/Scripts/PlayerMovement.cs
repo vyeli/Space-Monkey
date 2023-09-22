369aeab09e06d7ab2c514ee33501bae02a2640aa
VALENTIN YE LI <yeliv9527@gmail.com>
2023-09-22 01:48:28 -0300

    Modified free look camera, Ground textures pack

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,37 +18,39 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     #region UNITY_EVENTS
-    // Start is called before the first frame update
+    
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         float yStore = moveDirection.y;
 
+        // move
         moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
         moveDirection.Normalize();
         moveDirection *= moveSpeed;
 
         moveDirection.y = yStore;
 
+        // jump
         if (charController.isGrounded) { 
             if (Input.GetButtonDown("Jump"))
             {
                 moveDirection.y = jumpForce;
             }
         } else {
-            moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+            moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale; 
         }
 
         charController.Move(moveDirection * Time.deltaTime);
 
-        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0)
+        if(Input.GetAxis("Horizontal") != 0 || Input.GetAxis("Vertical") != 0) // if we are moving
         {
-            transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f);
+            transform.rotation = Quaternion.Euler(0f, playerCamera.transform.rotation.eulerAngles.y, 0f); // rotate the player to the camera's rotation
             Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
             playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
         }
