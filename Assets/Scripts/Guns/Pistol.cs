c14c221fe4f732cb705b5f681367d3de0c337977
VALENTIN YE LI <yeliv9527@gmail.com>
2023-10-05 23:35:41 -0300

    fix bullet position respawn

--- Assets/Scripts/Guns/Pistol.cs
+++ Assets/Scripts/Guns/Pistol.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Pistol : Gun
 {
+
+    [SerializeField] Transform muzzle;
     public override void Shoot()
     {
         if (_currentBulletCount == 0)
             return;
         
-        GameObject bullet = Instantiate(BulletPrefab, transform.position, Rotation, BulletContainer);
+        GameObject bullet = Instantiate(BulletPrefab, muzzle.position, Rotation, BulletContainer);
         bullet.GetComponent<Bullet>().SetOwner(this);
             
         UpdateBulletCount();
