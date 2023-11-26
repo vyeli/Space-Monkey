4d118b8e3961cf66e4c351fb75b52c3de3172a81
VALENTIN YE LI <yeliv9527@gmail.com>
2023-11-26 18:31:14 -0300

    fix: winning platform now with raycast detection
    vertically jumping above it will result in scene change

--- Assets/Scripts/Entities/Characters/Player.cs
+++ Assets/Scripts/Entities/Characters/Player.cs
@@ -105,9 +105,9 @@ public class Player : Actor
         {
             rayHit = hit.point;
             if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Killzone"))
-            {
                 GameManager.instance.RespawnPlayer();
-            }
+            else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Victoryzone"))
+                EventsManager.instance.EventGameOver(true);
             return true;
         }
         return false;
